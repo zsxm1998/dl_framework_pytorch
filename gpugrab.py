50eb1264309abb82398f87d351398687bd929d8a
zsxm1998 <zsxm1998@qq.com>
2023-09-27 09:06:43 +0000

    修改grab GPU代码

--- gpugrab.py
+++ gpugrab.py
@@ -6,9 +6,7 @@ from typing import Callable, Iterable
 import pynvml
 import time
 
-
 MEMORY_NUM = 4
-
 
 class TaskRunner:
     """
@@ -34,8 +32,14 @@ class TaskRunner:
         self.pool.terminate()
         return results
 
+def get_mem_used(pid, handle):
+    proinfos = pynvml.nvmlDeviceGetComputeRunningProcesses_v3(handle)
+    for pinfo in proinfos:
+        if pinfo.pid == pid:
+            return pinfo.usedGpuMemory
+    return 0
 
-def grab_gpu(device_id, nc):
+def grab_gpu(device_id, nc, gr):
     os.environ['CUDA_DEVICE_ORDER'] = "PCI_BUS_ID"
     #os.environ['CUDA_VISIBLE_DEVICES'] = f'{device_id}'
     device = torch.device(f'cuda:{device_id}')
@@ -47,23 +51,13 @@ def grab_gpu(device_id, nc):
     while True:
         meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
         free_memory = meminfo.free/1024**2
-        if free_memory > MEMORY_NUM * 100:
-            print(f'{time.strftime("%m-%d_%H:%M:%S", time.localtime())}: PID {pid} GPU {device_id} get {free_memory}MB free memory, grabing it.')
-            while True:
-                try:
-                    tensor_list.append( torch.zeros(5120, 5120, MEMORY_NUM, dtype=torch.float32, device=device) )
-                except RuntimeError:
-                    break
+        if free_memory > MEMORY_NUM * 100 and (gr is None or get_mem_used(pid, handle)/1024**2 < gr):
+            print(f'{time.strftime("%m-%d_%H:%M:%S", time.localtime())}: PID {pid} GPU {device_id} get {free_memory}MB free memory, grabing {"it" if gr is None else gr}.')
+            while pynvml.nvmlDeviceGetMemoryInfo(handle).free/1024**2 > MEMORY_NUM * 100 and (gr is None or get_mem_used(pid, handle)/1024**2 < gr):
+                tensor_list.append( torch.zeros(5120, 5120, MEMORY_NUM, dtype=torch.float32, device=device) )
         elif not nc:
-            proinfos = pynvml.nvmlDeviceGetComputeRunningProcesses_v3(handle)
-            flag = False
-            usedGpuMemory = -1
-            for pinfo in proinfos:
-                if pinfo.pid == pid and pinfo.usedGpuMemory / meminfo.total > 0.9:
-                    usedGpuMemory = pinfo.usedGpuMemory
-                    flag = True
-                    break
-            if flag and len(tensor_list) > 0:
+            usedGpuMemory = get_mem_used(pid, handle)
+            if usedGpuMemory / meminfo.total > 0.9 and len(tensor_list) > 0:
                 if not compute_flag:
                     print(f'{time.strftime("%m-%d_%H:%M:%S", time.localtime())}: PID {pid} GPU {device_id} {usedGpuMemory/1024**2}/{meminfo.total/1024**2}={usedGpuMemory/meminfo.total}, start computing.')
                     compute_flag = True
@@ -73,15 +67,14 @@ def grab_gpu(device_id, nc):
                     time.sleep(1)
         time.sleep(1)
 
-
 def get_args():
     parser = argparse.ArgumentParser(description='Hold GPU memory.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--device', '-d', type=str, default=None, help='GPU index')
-    parser.add_argument('--notcompute', '-nc', action='store_true', help='not use GPU to compute')
+    parser.add_argument('--notcompute', '-n', action='store_true', help='not use GPU to compute')
+    parser.add_argument('--grab', '-g', type=int, default=None, help='Max grab memory (MB)')
 
     return parser.parse_args()
-
 
 if __name__ == '__main__':
     opt = get_args()
@@ -90,5 +83,5 @@ if __name__ == '__main__':
     else:
         device_ids = [int(i) for i in opt.device.replace(' ', '').split(',')]
 
-    kwargs = [{'device_id': i, 'nc': opt.notcompute} for i in device_ids]
+    kwargs = [{'device_id': i, 'nc': opt.notcompute, 'gr': opt.grab} for i in device_ids]
     TaskRunner().run(grab_gpu, kwargs)
